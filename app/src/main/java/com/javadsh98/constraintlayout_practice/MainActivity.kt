4733f1b502afbc464f7bb162a821cf098afad150
MJavad98 <mohammad.javad.shabani.1377.3.24@gmail.com>
2020-06-10 12:10:07 +0430

    add MovieActivity.kt

--- app/src/main/java/com/javadsh98/constraintlayout_practice/MainActivity.kt
+++ app/src/main/java/com/javadsh98/constraintlayout_practice/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.javadsh98.constraintlayout_practice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_movie.setOnClickListener{ _ -> startActivity(Intent(this, MovieActivity::class.java))}
+        
     }
 }
---
+++ app/src/main/java/com/javadsh98/constraintlayout_practice/MovieActivity.kt
@@ -0,0 +1,11 @@
+package com.javadsh98.constraintlayout_practice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MovieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie)
+    }
+}
